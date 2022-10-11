291f478273f1fc8f221dd25f81242229b4cb0576
bgarkow <115035059+bgarkow@users.noreply.github.com>
2022-10-11 10:54:18 -0600

    Update sys_stats.sh

--- sys_stats.sh
+++ sys_stats.sh
@@ -1,5 +1,5 @@
-# add shebang here
-# add any contributor names in a comment here
+#!/bin/bash
+# Brandon Garkow
 
 #************************************#
 #******* OUTPUT SYSTEM STATS ********#
@@ -8,27 +8,40 @@
 # Make a directory called System_Stats and change directories to that directory
 # Try to make the output for each file look as neat and organized as you can.
 
+mkdir System_Stats
+cd System_Stats
+
+
 # Output the following information to a file called kernel
     # 1. Kernel Name
     # 2. Kernel Release
     # 3. Kernel Version
     # 4. The operating system
-
+    
+uname -s > kernel
+uname -r >> kernel
+uname -v >> kernel
+uname -o >> kernel
 
 # Output the following information to a file called network
     # 1. Do not print any serial numbers or sensitive information related to the system
     # 2. All network interfaces
+ip link show > network
     
     
 # Output the following information to a file called disk.html
     # 1. All disks
     # 2. The output should include html tags, i.e. <html></html>
+    
+sudo lshw -html > disk.html
 
 
 # Output the following information to a file called cpu
     # 1. The first five lines of the command lscpu
     # 2. The last 12 lines of the command lscpu
     
+lscpu | head -n5 > cpu
+lscpu | tail -n12 >> cpu
 
 # Output the following information to a file called block_dev
     # 1. Only the name, size, and type of the block devices
@@ -37,5 +50,4 @@
     
 # Output the following information to a file called sata
     # 1. Any Sata devices connected to the machine along with human readable sizes of the devices
-    # 2. More specific information about each device that is connected to the machine
-    
+    # 2. More specific information about each device that is connected to the machin
